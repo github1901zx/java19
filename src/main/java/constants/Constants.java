a22388954bb268ed81f2e1b90d280228ea94b57f
asus <h0c0kutaiiok@gmail.com>
2021-08-31 19:53:44 +0300

    31.08.21

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,11 +1,11 @@
 package constants;
 
 public interface Constants {
-////    local
+//  local
     String url = "jdbc:mysql://localhost:7777/students_java_19";
     String user = "root";
     String password = "root19011994";
-////  remove
+//  remove
 //    String url = "jdbc:mysql://localhost:3306/students_java_19";
 //    String user = "root19";
 //    String password = "root19011994";
